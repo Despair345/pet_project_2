6ca1617f799003f1d4535b54a57c079c8431b359
marykk-lab <maksimos0899@gmail.com>
2025-04-11 11:28:30 +0200

    urls, template changes updating products

--- games/forms.py
+++ games/forms.py
@@ -1,5 +1,14 @@
 from django import forms
 from .models import Order, Game
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class SignUpForm(UserCreationForm):
+    class Meta():
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
 
 class OrderForm(forms.ModelForm):
     class Meta:
--- games/views.py
+++ games/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from .models import Game, Order
-from .forms import OrderForm, GameForm
+from .forms import OrderForm, GameForm, SignUpForm
 from .cart import Cart
 from django.http import JsonResponse
+
+def register(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'register.html', {'form': form})
 
 def index(request):
     return render(request, 'home.html')
